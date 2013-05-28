5a15fab82ed07b1a2b7357de119f108dbd00396a
pixslx <zoran.krleza@gmail.com>
2013-05-28 19:09:04 +0200

    formatting

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -2,18 +2,14 @@
 #include <inttypes.h>
 #include <compat/twi.h>
 
-void I2C::init(void)
-{
-  /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
-  
-  TWSR = 0;                         /* no prescaler */
-  TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
-
+void I2C::init(void) {
+	/* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
+	TWSR = 0;                         /* no prescaler */
+	TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
 }
 
-unsigned char I2C::start(unsigned char address)
-{
-    uint8_t   twst;
+unsigned char I2C::start(unsigned char address) {
+	uint8_t   twst;
 
 	// send START condition
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
@@ -23,7 +19,7 @@ unsigned char I2C::start(unsigned char address)
 
 	// check value of TWI Status Register. Mask prescaler bits.
 	twst = TW_STATUS & 0xF8;
-	if ( (twst != TW_START) && (twst != TW_REP_START)) return 1;
+	if ((twst != TW_START) && (twst != TW_REP_START)) return 1;
 
 	// send device address
 	TWDR = address;
@@ -37,70 +33,61 @@ unsigned char I2C::start(unsigned char address)
 	if ( (twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK) ) return 1;
 
 	return 0;
-
 }
 
 void I2C::start_wait(unsigned char address)
 {
-    uint8_t   twst;
+	uint8_t twst;
+	while (1) {
+		// send START condition
+		TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+    
+		// wait until transmission completed
+		while(!(TWCR & (1<<TWINT)));
+    
+		// check value of TWI Status Register. Mask prescaler bits.
+		twst = TW_STATUS & 0xF8;
+		if ((twst != TW_START) && (twst != TW_REP_START)) continue;
+    
+		// send device address
+		TWDR = address;
+		TWCR = (1<<TWINT) | (1<<TWEN);
+    
+		// wail until transmission completed
+		while(!(TWCR & (1<<TWINT)));
+    
+		// check value of TWI Status Register. Mask prescaler bits.
+		twst = TW_STATUS & 0xF8;
+		if ((twst == TW_MT_SLA_NACK )||(twst ==TW_MR_DATA_NACK)) {    	    
+			/* device busy, send stop condition to terminate write operation */
+			TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 
-
-    while ( 1 )
-    {
-	    // send START condition
-	    TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
-    
-    	// wait until transmission completed
-    	while(!(TWCR & (1<<TWINT)));
-    
-    	// check value of TWI Status Register. Mask prescaler bits.
-    	twst = TW_STATUS & 0xF8;
-    	if ( (twst != TW_START) && (twst != TW_REP_START)) continue;
-    
-    	// send device address
-    	TWDR = address;
-    	TWCR = (1<<TWINT) | (1<<TWEN);
-    
-    	// wail until transmission completed
-    	while(!(TWCR & (1<<TWINT)));
-    
-    	// check value of TWI Status Register. Mask prescaler bits.
-    	twst = TW_STATUS & 0xF8;
-    	if ( (twst == TW_MT_SLA_NACK )||(twst ==TW_MR_DATA_NACK) ) 
-    	{    	    
-    	    /* device busy, send stop condition to terminate write operation */
-	        TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+			// wait until stop condition is executed and bus released
+			while(TWCR & (1<<TWSTO));
 	        
-	        // wait until stop condition is executed and bus released
-	        while(TWCR & (1<<TWSTO));
-	        
-    	    continue;
-    	}
-    	//if( twst != TW_MT_SLA_ACK) return 1;
-    	break;
-     }
-
+			continue;
+		}
+		break;
+	}
 }
 
 unsigned char I2C::rep_start(unsigned char address)
 {
-    return start( address );
-
+	return start( address );
 }
 
 void I2C::stop(void)
 {
-    /* send stop condition */
+	/* send stop condition */
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 	
 	// wait until stop condition is executed and bus released
 	while(TWCR & (1<<TWSTO));
-
 }
 
 unsigned char I2C::write( unsigned char data )
 {	
-    uint8_t   twst;
+	uint8_t   twst;
     
 	// send data to the previously addressed device
 	TWDR = data;
@@ -113,7 +100,6 @@ unsigned char I2C::write( unsigned char data )
 	twst = TW_STATUS & 0xF8;
 	if( twst != TW_MT_DATA_ACK) return 1;
 	return 0;
-
 }
 
 unsigned char I2C::readAck(void)
@@ -121,8 +107,7 @@ unsigned char I2C::readAck(void)
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWEA);
 	while(!(TWCR & (1<<TWINT)));    
 
-    return TWDR;
-
+	return TWDR;
 }
 
 unsigned char I2C::readNak(void)
@@ -130,6 +115,5 @@ unsigned char I2C::readNak(void)
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	while(!(TWCR & (1<<TWINT)));
 	
-    return TWDR;
-
+	return TWDR;
 }
